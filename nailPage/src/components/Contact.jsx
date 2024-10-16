9ed72867bb6efae2a7fe50e00b904e7e083e3e39
kwrona05 <kwrona05@gmail.com>
2024-10-16 20:57:22 +0200

    set contact ulList

---
+++ nailPage/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div>
+      <ul>
+        <li>evellabella@gmail.com</li>
+        <li>333 444 555</li>
+      </ul>
+    </div>
+  );
+};
+export default Contact;
