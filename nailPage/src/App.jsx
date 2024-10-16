9ec0baf98c6e384117c3f5cbedcb8259e14b3bdc
kwrona05 <kwrona05@gmail.com>
2024-10-16 20:55:55 +0200

    Configure routing

--- nailPage/src/App.jsx
+++ nailPage/src/App.jsx
@@ -1,3 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
 const App = () => {
-  return <div className="container"></div>;
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>;
 };
