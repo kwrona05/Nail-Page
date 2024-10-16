b4a406bdacb639c303201a608bdd2c015a6f479b
kwrona05 <kwrona05@gmail.com>
2024-10-16 20:56:47 +0200

    set imgs in gallery

---
+++ nailPage/src/components/Gallery.jsx
@@ -0,0 +1,12 @@
+const Gallery = () => {
+  return (
+    <div className="gallery-div">
+      <img src="" alt="nail" />
+      <img src="" alt="nail2" />
+      <img src="" alt="nail3" />
+      <img src="" alt="nail4" />
+      <img src="" alt="nail5" />
+    </div>
+  );
+};
+export default Gallery;
