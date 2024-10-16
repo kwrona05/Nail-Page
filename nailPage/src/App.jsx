5d37828f65d4f896d4d28d2bf336bf8ed46a8c9c
kwrona05 <kwrona05@gmail.com>
2024-10-16 22:02:54 +0200

    Add route path='*'
    When the source is unknown you will be redirect to Home component

--- nailPage/src/App.jsx
+++ nailPage/src/App.jsx
@@ -1,13 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./components/Home";
 import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/gallery" element={<Gallery />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  </Router>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 };
+
+export default App;
